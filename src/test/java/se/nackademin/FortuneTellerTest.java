5e2c80ef790cedc7d1f5f929c8c5fb22f46219ac
Jesper Hägglund <jeppan875@gmail.com>
2017-04-02 14:24:17 +0200

    FortuneTeller class 100% code coverage

--- src/test/java/se/nackademin/FortuneTellerTest.java
+++ src/test/java/se/nackademin/FortuneTellerTest.java
@@ -65,5 +65,74 @@ public class FortuneTellerTest {
         FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
         boolean result = fortuneTeller.setLocation("Stockholm");
         assertTrue("setLocation should return true for valid input",result);        
-    }    
+    } 
+            @Test
+    public void testSetNameWithNullInput() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setName(null);
+        assertFalse("setLocation should return false for null input",result);
+    }        
+        @Test
+    public void testSetNameWithEmptyInput() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setName("");
+        assertFalse("setName should return false for empty input",result);        
+    }
+        @Test
+    public void testSetNameWithValidInput() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setName("Jesper");
+        assertTrue("setName should return true for valid input",result);        
+    }
+        @Test
+    public void testSetHeight() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setHeight("189");
+        assertTrue("should return true for valid input", result);
+        verify(magicNumbersMock, times(1)).setHeight(189);
+    }
+
+    @Test
+    public void testSetHeightWithInvalidInput() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setHeight("not an integer");
+        assertFalse("should return false for invalid input", result);
+    }
+        @Test
+    public void testSetIncome() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setIncome("25000");
+        assertTrue("should return true for valid input", result);
+        verify(magicNumbersMock, times(1)).setIncome(25000);
+    }
+            @Test
+    public void testSetIncomeMax() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setIncome("10000001");
+        assertFalse("should return false for valid input", result);
+        verify(magicNumbersMock, times(1)).setIncome(10000001);
+    }
+
+    @Test
+    public void testSetIncomeWithInvalidInput() {
+        MagicNumbers magicNumbersMock = mock(MagicNumbers.class);
+        Translator translatorMock = mock(Translator.class);
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbersMock, translatorMock);
+        boolean result = fortuneTeller.setIncome("not an integer");
+        assertFalse("should return false for invalid input", result);
+    }
 }
